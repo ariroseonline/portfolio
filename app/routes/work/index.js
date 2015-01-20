85989a09347b2c54b7113f06e11f74070f92d3af
ariroseonline <ariroseonline@gmail.com>
2015-01-20 02:04:01 -0500

    removed alert

--- app/routes/work/index.js
+++ app/routes/work/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	redirect: function(model){
-		alert('redirect index')
 		this.transitionTo('company', model.get('firstObject.slug'));
 	}
 });
